cf84b9db89488da903ffdf359c98a217f093aea4
Christopher Patchett <chrispymiracle@gmail.com>
2023-04-13 13:51:18 -0600

    created modal to congratulate winning play, also redirects

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -1,14 +1,17 @@
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { View, Alert, Modal, Text, StyleSheet, Button } from 'react-native';
 import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import { GameRow } from './GameRow';
 import { randArrOfNine } from '../utils/randArrOfNine';
 
 export const Gameboard = (props) => {
+    const navigation = useNavigation()
     const initialEmptySquare = Math.floor((Math.random() * 8) + 1)
     const [emptySquare, setEmptySquare] = useState(initialEmptySquare)
     // TODO create random tile ordering... 
     const [orderOfTiles, setOrderOfTiles] = useState(randArrOfNine())
+    const [winner, setWinner] = useState(false)
 
     const rows = ['rowOne', 'rowTwo', 'rowThree']
 
@@ -16,28 +19,34 @@ export const Gameboard = (props) => {
         console.log("You pressed gamepiece", id)
         if (orderOfTiles.indexOf(id) + 1 === emptySquare || orderOfTiles.indexOf(id) - 1 === emptySquare || orderOfTiles.indexOf(id) + 3 === emptySquare || orderOfTiles.indexOf(id) - 3 === emptySquare) {
             
-            console.log(`Tile (${orderOfTiles.indexOf(id)}) is adjacent to empty square (${emptySquare})!  id=${id}`)
-            
+            // console.log(`Tile (${orderOfTiles.indexOf(id)}) is adjacent to empty square (${emptySquare})!  id=${id}`)
+        
             let tempOrderOfTiles = [...orderOfTiles]
-            console.log("temp: ", tempOrderOfTiles)
             let tempEmptySquare = tempOrderOfTiles[orderOfTiles.indexOf(id)]
-            console.log("pre(index of id): ", tempOrderOfTiles[orderOfTiles.indexOf(id)], orderOfTiles[emptySquare])
             tempOrderOfTiles[orderOfTiles.indexOf(id)] = orderOfTiles[emptySquare]
-            console.log("post(index of id): ", tempOrderOfTiles[orderOfTiles.indexOf(id)])
-            console.log("pre(index of empty): ", tempOrderOfTiles[emptySquare])
             tempOrderOfTiles[emptySquare] = tempEmptySquare
-            console.log("final: ", tempOrderOfTiles)
+            console.log(tempOrderOfTiles)
             setEmptySquare(orderOfTiles.indexOf(id))
             setOrderOfTiles(tempOrderOfTiles)
-            
+            let orderedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            if (JSON.stringify(tempOrderOfTiles) === JSON.stringify(orderedArray)) {
+                console.log("You Win!!!")
+                setEmptySquare(null)
+                setWinner(true)
+            }
+
         }
         else {
             alert(`Tile (${orderOfTiles.indexOf(id)}) not adjacent to empty square (${emptySquare})!  id=${id}`)
         }
+
     }
 
+
     return (
+        
         <View style={styles.container}>
+            
             {rows.map((item, key) => (
                 <GameRow 
                     id={item}
@@ -47,6 +56,23 @@ export const Gameboard = (props) => {
                     image={props.image}
                     orderOfTiles={orderOfTiles}/>
             ))}
+            {winner &&  
+            <Modal
+                style={styles.modal}
+                animationType="slide"
+                visible={winner}
+                transparent={true}
+                onRequestClose={() => {
+                    Alert.alert('Modal has been closed.');
+                    // setModalVisible(!modalVisible);
+                  }}>
+                    <View style={styles.modalCont}>
+                        <Text style={styles.text}>You Won!!!</Text> 
+                        <Button title="Go back" onPress={() => navigation.navigate('Start a Game')} /> 
+                    </View>
+                  
+            </Modal>
+            }
         </View>
     )
 }
@@ -64,4 +90,25 @@ const styles = StyleSheet.create({
         columnGap: 2,
         justifyContent: 'space-around'
     },
+    text: {
+        color: 'white',
+        fontSize: 50,
+    },
+    // modal: {
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    // },
+    modalCont: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 100,
+        width: 300,
+        backgroundColor: 'teal',
+        top: 100,
+        left: 35,
+        padding: 15,
+        borderRadius: 10,
+        borderColor: 'black',
+        borderWidth: 2,
+    }
 })
